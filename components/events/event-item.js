fe210a6626c7e8c1409907e7422e70405f82e54b
Mohammad Bayati <Mohammad.Bayati@SLATERGORDON.UK>
2022-12-16 12:55:06 +0000

    Optimizing NextJS App

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -3,6 +3,7 @@ import classes from "./event-item.module.css";
 import AddressIcon from "../icons/address-icon";
 import ArrowRightIcon from "../icons/arrow-right-icon";
 import DateIcon from "../icons/date-icon";
+import Image from "next/image";
 
 export default function EventItem({ title, image, date, location, id }) {
   const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
@@ -14,7 +15,7 @@ export default function EventItem({ title, image, date, location, id }) {
   const exploreLink = `/events/${id}`;
   return (
     <li className={classes.item}>
-      <img src={`/${image}`} alt={title} />
+      <Image src={`/${image}`} alt={title} width={240} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
